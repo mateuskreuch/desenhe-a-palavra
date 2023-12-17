4727e41db60c132d3d338d5203aedae56140b340
Mateus A. Kreuch <mateus.kreuch@gmail.com>
2023-12-16 23:18:29 -0300

    quick

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ let currentPlayer = 0;
 let isImposter = [];
 let word = '';
 let definition = '';
+let impostorWord = '';
+let impostorDefinition = '';
 let currentScreen = 0;
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -78,6 +80,10 @@ function showWord() {
          document.getElementById('wordLabel').innerHTML = word;
          document.getElementById('definitionLabel').innerHTML = definition;
       }
+      else {
+         document.getElementById('wordLabel').innerHTML = impostorWord;
+         document.getElementById('definitionLabel').innerHTML = impostorDefinition;
+      }
    });
 
    return false;
@@ -107,6 +113,30 @@ async function fetchWord() {
 
    definition = definitions.join(' ');
    definition = definition.replaceAll(/_(.*?)_/g, "<i>$1</i>");
+
+   impostorWord = await fetch(DICTIONARY_API + '/random');
+   impostorWord = await impostorWord.json();
+   impostorWord = impostorWord.word;
+
+   impostorDefinition = await fetch(DICTIONARY_API + '/word/' + impostorWord);
+   impostorDefinition = await impostorDefinition.json();
+   impostorDefinition = impostorDefinition[0].xml;
+   impostorDefinition = impostorDefinition.replaceAll('\n', ' ');
+
+   definitions = impostorDefinition.match(/<def>.*?<\/def>/g);
+
+   for (let i = 0; i < definitions.length; i++) {
+      definitions[i] = definitions[i].slice(5, -6).trim();
+
+      // if there is any XML tag remaining, try again
+      if (definitions[i].search(/<.*?>/g) >= 0) {
+         fetchWord();
+         return;
+      }
+   }
+
+   impostorDefinition = definitions.join(' ');
+   impostorDefinition = impostorDefinition.replaceAll(/_(.*?)_/g, "<i>$1</i>");
 
    showWord();
 }
